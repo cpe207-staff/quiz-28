9caf62404d3e9f509f5d338fb39e6dad002710d6
minyjae <120163272+minyjae@users.noreply.github.com>
2024-08-08 15:27:15 +0700

    Update Comment.tsx

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,15 +1,34 @@
 "use client";
+import { CommentProps } from "@/libs/types";
+import { ReplyProps } from "@/libs/types";
 export default function Comment({
   ImagePath,
   username,
   commentTitle,
   likes,
   replies,
-}) {
-  return (
-    <div>
-      your code for Comment component here ...
-      {/* You can use map-loop to render Reply component here */}
-    </div>
+}: CommentProps) {
+  return (  
+    <div className="d-flex gap-2 my-2">
+          <img
+            src= {ImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{username}</span>
+            <br />
+            <span>{commentTitle}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span className="text-muted">{likes} คน</span>
+            </div>
+          </div>
+        </div>
   );
 }
